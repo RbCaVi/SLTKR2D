05bd619a24f44195b0c200d76fb76b9c6619c3e5
RbCaViTerminal <robert@robertvail.info>
2024-03-15 12:14:44 -0400

    other ways to get token

--- pyfunc/gettoken.py
+++ pyfunc/gettoken.py
@@ -1,8 +1,26 @@
+import lang
+import os
+
 def gettoken():
+	errs=[]
 	try:
+		# dotenv file
 		from dotenv import dotenv_values
-		print("test")
-		return dotenv_values("cred/.env")['TOKEN']
-		
-	except:
-		print("Error on Loading TOKEN.")
+		return dotenv_values(lang.cfg("TOKENPATH"))['TOKEN']
+	except Exception as e:
+		errs.append(e)
+	try:
+		# token in main directory
+		with open("token") as f:
+			token=f.read()
+		return token
+	except Exception as e:
+		errs.append(e)
+	try:
+		# token in environment
+		return os.environ['token']
+	except Exception as e:
+		errs.append(e)
+	raise Exception("Error on Loading TOKEN.",errs)
+
+token=gettoken()
